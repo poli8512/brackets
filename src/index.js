0babcb1ecbfa274324bdd3e96a50080cc7a3154c
poli8512 <69201867+poli8512@users.noreply.github.com>
2021-02-24 09:52:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strArr=str.split('')
+    let stack=[]
+    let mapBr= new Map([...bracketsConfig])
+    let newA=strArr
+    let rec=(ar,mp)=>{
+        let j=0
+        do{
+            // console.log(ar[j])
+            for(let [o,c] of mapBr){
+                if(ar[j]==o && ar[j+1]==c){
+                    newA.splice(j,2)
+                    console.log(newA)
+                    rec(newA,mapBr)
+
+                }
+            }
+            j++
+        }
+        while(j<ar.length)
+
+    }
+
+    rec(strArr,mapBr)
+    console.log("NEW",newA)
+    if(!newA || JSON.stringify(newA).length>3){
+        return false
+    }else{
+        return true
+    }
 }
